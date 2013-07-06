f9a73d5d6e3aeea1350f26500706bdbb2f1685ad
yuya373 <yuya373@me.com>
2013-07-07 07:50:34 +0900

    delete migrations

--- db/migrate/20130629070158_change_type_from_batter_detail.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTypeFromBatterDetail < ActiveRecord::Migration
-  def change
-    rename_column :batter_details, :type, :pitch_type
-  end
-end
--- db/migrate/20130629212834_add_p_b_to_batter_detail.rb
+++
@@ -1,5 +0,0 @@
-class AddPBToBatterDetail < ActiveRecord::Migration
-  def change
-    add_column :batter_details, :p_b, :string
-  end
-end
