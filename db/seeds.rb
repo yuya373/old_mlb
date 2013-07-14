da53987db2bac5e0887987aea3284b0bdc886630
yuya373 <yuya373@me.com>
2013-07-14 18:13:39 +0900

    seed pitching

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ require 'csv'
 # Pitching.delete_all
 
 
-pitching = CSV.table('db/pitching_1_1.csv')
+pitching = CSV.table('db/pitching_1_2.csv')
 pitching_att = pitching.headers
 pitching.each do |pitching|
   @pitching = {}
