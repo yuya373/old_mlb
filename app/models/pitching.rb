a12459b9a422ac529c2c707a9e107643cb73c420
yuya373 <yuya373@me.com>
2013-07-17 22:31:55 +0900

    seed pitching

--- app/models/pitching.rb
+++ app/models/pitching.rb
@@ -82,51 +82,50 @@ class Pitching < ActiveRecord::Base
   end
 
   def self.seed(gid)
-      year = gid.slice(0,4)
-      month = gid.slice(5,2)
-      day = gid.slice(8,2)
-      # 2013/06/01/wasmlb-atlmlb-1
-      away_team = gid.slice(11,6)
-      home_team = gid.slice(18,6)
-      num = gid.slice(25,1)
+    year = gid.slice(0,4)
+    month = gid.slice(5,2)
+    day = gid.slice(8,2)
+    # 2013/06/01/wasmlb-atlmlb-1
+    away_team = gid.slice(11,6)
+    home_team = gid.slice(18,6)
+    num = gid.slice(25,1)
 
-      url = "http://gd2.mlb.com/components/game/mlb/year_#{year}/month_#{month}/day_#{day}/gid_#{year}_#{month}_#{day}_#{away_team}_#{home_team}_#{num}/inning/inning_all.xml"
-      begin
-        doc = Nokogiri::XML(open(url))
+    url = "http://gd2.mlb.com/components/game/mlb/year_#{year}/month_#{month}/day_#{day}/gid_#{year}_#{month}_#{day}_#{away_team}_#{home_team}_#{num}/inning/inning_all.xml"
+    begin
+      doc = Nokogiri::XML(open(url))
 
-        atbat = doc.css('atbat')
+      atbat = doc.css('atbat')
 
 
-        atbat.each do |atbat|
-          atbat_num = atbat.attribute('num').text
-          p = atbat.attribute('pitcher').text
-          b = atbat.attribute('batter').text
-          atbat.css('pitch').each do |pitch|
-            @pitching = {}
-            @pitching[:game_id] = gid
-            @pitching[:num] = atbat_num
-            @pitching[:pitcher_id] = p
-            @pitching[:batter_id] = b
-            @pitching[:game_id_num] = "#{gid}_#{atbat_num}"
-            pitch.keys.to_a.each do |k|
-              v = pitch.attribute(k).text
-              case k
-              when 'id'
-              when 'type'
-                k = 's_or_ball'
-                @pitching[k.to_sym] = v
-              else
-                @pitching[k.to_sym] = v
-              end
-            end
-            begin
-              Pitching.where('sv_id = ?', @pitching[:sv_id]).first.update_attributes!(@pitching)
-            rescue
-              Pitching.create(@pitching)
+      atbat.each do |atbat|
+        atbat_num = atbat.attribute('num').text
+        p = atbat.attribute('pitcher').text
+        b = atbat.attribute('batter').text
+        atbat.css('pitch').each do |pitch|
+          @pitching = {}
+          @pitching[:game_id] = gid
+          @pitching[:num] = atbat_num
+          @pitching[:pitcher_id] = p
+          @pitching[:batter_id] = b
+          @pitching[:game_id_num] = "#{gid}_#{atbat_num}"
+          pitch.keys.to_a.each do |k|
+            v = pitch.attribute(k).text
+            case k
+            when 'id'
+            when 'type'
+              k = 's_or_ball'
+              @pitching[k.to_sym] = v
+            else
+              @pitching[k.to_sym] = v
             end
           end
-        rescue
+          begin
+            Pitching.where('sv_id = ?', @pitching[:sv_id]).first.update_attributes!(@pitching)
+          rescue
+            Pitching.create(@pitching)
+          end
         end
-      end
+    rescue
     end
+  end
 end
