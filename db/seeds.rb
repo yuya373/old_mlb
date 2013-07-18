434c88a6bda45f475148a8d52d3b77f64ecd7077
yuya373 <yuya373@me.com>
2013-07-18 20:53:50 +0900

    seed from csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,86 +12,89 @@ require 'open-uri'
 require 'csv'
 
 
-
-
-
-
-
-
-game_id = []
-
-  url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_03/day_31/master_scoreboard.xml"
-  begin
-    doc = Nokogiri::XML(open(url))
-    doc.css('game').each do |game|
-      game_id << game.attribute('gameday').text
-    end
-  rescue
-  end
-
-num_4 = 1.upto(30).to_a
-num_4.each do |num|
-  day = sprintf("%.2d",num)
-  url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_04/day_#{day}/master_scoreboard.xml"
-  begin
-    doc = Nokogiri::XML(open(url))
-    doc.css('game').each do |game|
-      game_id << game.attribute('gameday').text
-    end
-  rescue
-    next
-  end
-end
-
-num_5 = 1.upto(31).to_a
-num_5.each do |num|
-  day = sprintf("%.2d",num)
-  url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_05/day_#{day}/master_scoreboard.xml"
-  begin
-    doc = Nokogiri::XML(open(url))
-    doc.css('game').each do |game|
-      game_id << game.attribute('gameday').text
-    end
-  rescue
-    next
-  end
-end
-
-num_6 = 1.upto(30).to_a
-num_6.each do |num|
-  day = sprintf("%.2d",num)
-  url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_06/day_#{day}/master_scoreboard.xml"
-  begin
-    doc = Nokogiri::XML(open(url))
-    doc.css('game').each do |game|
-      game_id << game.attribute('gameday').text
-    end
-  rescue
-    next
-  end
-end
-
-num_7 = 1.upto(14).to_a
-num_7.each do |num|
-  day = sprintf("%.2d",num)
-  url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_07/day_#{day}/master_scoreboard.xml"
-  begin
-    doc = Nokogiri::XML(open(url))
-    doc.css('game').each do |game|
-      game_id << game.attribute('gameday').text
-    end
-  rescue
-    next
-  end
-end
-
-game_id.each do |gid|
+CSV.foreach("game_id.csv") do |gid|
   Atbat.seed(gid)
+end
+
+
+
+
+
+# game_id = []
+
+#   url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_03/day_31/master_scoreboard.xml"
+#   begin
+#     doc = Nokogiri::XML(open(url))
+#     doc.css('game').each do |game|
+#       game_id << game.attribute('gameday').text
+#     end
+#   rescue
+#   end
+
+# num_4 = 1.upto(30).to_a
+# num_4.each do |num|
+#   day = sprintf("%.2d",num)
+#   url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_04/day_#{day}/master_scoreboard.xml"
+#   begin
+#     doc = Nokogiri::XML(open(url))
+#     doc.css('game').each do |game|
+#       game_id << game.attribute('gameday').text
+#     end
+#   rescue
+#     next
+#   end
+# end
+
+# num_5 = 1.upto(31).to_a
+# num_5.each do |num|
+#   day = sprintf("%.2d",num)
+#   url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_05/day_#{day}/master_scoreboard.xml"
+#   begin
+#     doc = Nokogiri::XML(open(url))
+#     doc.css('game').each do |game|
+#       game_id << game.attribute('gameday').text
+#     end
+#   rescue
+#     next
+#   end
+# end
+
+# num_6 = 1.upto(30).to_a
+# num_6.each do |num|
+#   day = sprintf("%.2d",num)
+#   url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_06/day_#{day}/master_scoreboard.xml"
+#   begin
+#     doc = Nokogiri::XML(open(url))
+#     doc.css('game').each do |game|
+#       game_id << game.attribute('gameday').text
+#     end
+#   rescue
+#     next
+#   end
+# end
+
+# num_7 = 1.upto(14).to_a
+# num_7.each do |num|
+#   day = sprintf("%.2d",num)
+#   url = "http://gd2.mlb.com/components/game/mlb/year_2013/month_07/day_#{day}/master_scoreboard.xml"
+#   begin
+#     doc = Nokogiri::XML(open(url))
+#     doc.css('game').each do |game|
+#       game_id << game.attribute('gameday').text
+#     end
+#   rescue
+#     next
+#   end
+# end
+
+# game_id.each do |gid|
+#   gid.tr('/','_')
+#   Atbat.seed(gid)
   # Pitching.seed(gid)
   # Game.seed(gid)
   # GamePitcher.seed(gid)
   # GameBatter.seed(gid)
   # LineScore.seed(gid)
-end
+# end
 
 
