8ea5277077147cba5637ed95b3107935cb71dfa9
yuya373 <yuya373@me.com>
2013-08-04 00:41:15 +0900

    add validation to favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,8 @@ class Favorite < ActiveRecord::Base
   belongs_to :team, foreign_key: 'team_id'
   belongs_to :pitcher, foreign_key: 'p_id'
   belongs_to :batter, foreign_key: 'b_id'
+
+  validates_uniqueness_of :p_id, scope: [:c_type, :user_id], message: "同じ選手は重複して選べません"
+  validates_uniqueness_of :b_id, scope: [:c_type, :user_id], message: "同じ選手は重複して選べません"
+  validates_uniqueness_of :team_id, scope: [:c_type, :user_id], message: "同じチームは重複して選べません"
 end
