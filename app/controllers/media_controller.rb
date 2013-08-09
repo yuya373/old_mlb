02b34af6ff1865db70bde2e91982abde4dbc5f41
yuya373 <yuya373@me.com>
2013-08-09 10:12:34 +0900

    cgがないとき

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,6 +2,6 @@ class MediaController < ApplicationController
  def show
   @g_id = params[:g_id]
   @media = Media.where(game_id: @g_id).order(:sv_id).where.not(media_type: 'condensed')
-  @cg = Media.where(game_id: @g_id).where(media_type: 'condensed').first
+  @cg = Media.where('game_id = ? and media_type = ?', @g_id, 'condensed')
  end
 end
